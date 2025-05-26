a62becd54ad04314caa0be5dfa24371da4919fe1
Tran Hai Dang <tranhaidang@Trans-MacBook-Pro.local>
2025-05-27 04:23:49 +0700

    final

--- lab-01/ex_02_08.py
+++ lab-01/ex_02_08.py
@@ -1,15 +1,18 @@
-def chia_chet_cho_5(so_nhi_phan):
-    so_thap_phan = int(so_nhi_phan,2)
-    if so_thap_phan = int(so_nhi_phan,2)
-    if so_thap_phan % 5 ==0:
+
+def chia_het_cho_5(so_nhi_phan):
+    so_thap_phan = int(so_nhi_phan, 2)
+    if so_thap_phan % 5 == 0:
         return True
-        else"
+    else:
         return False
-Chuoi_so_nhi_phan = input("Nhap chuoi so nhi phan(phan tach boi dau phay):")
-so_si_phan_list = Chuoi_so_nhi_phan.split(',')
-so_chia_het_cho_5 = [so for so in so_si_phan_list if chia_chet_cho_5(so)]
+
+chuoi_so_nhi_phan = input("Nhap chuoi so nhi phan (phan tach boi dau phay): ")
+
+so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
+so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5(so)]
+
 if len(so_chia_het_cho_5) > 0:
     ket_qua = ','.join(so_chia_het_cho_5)
-    print("Cac so nhi phan nao chia het cho 5 la:", ket_qua)
-    else:
-        print("Khong co so ")
+    print("Cac so nhi phan chia het cho 5 la:", ket_qua)
+else:
+    print("Khong co so nhi phan nao chia het cho 5 trong chuoi da nhap.")
